8d3ad817707703c11b5b7238b0312236a010dca3
Gregoire Clement <gregoire.clement@epfl.ch>
2018-09-20 23:39:41 +0200

    Functions in class Model created

--- rabbits/clement-delisle-in/src/RabbitsGrassSimulationModel.java
+++ rabbits/clement-delisle-in/src/RabbitsGrassSimulationModel.java
@@ -13,34 +13,111 @@ import uchicago.src.sim.engine.SimModelImpl;
 
 public class RabbitsGrassSimulationModel extends SimModelImpl {
 
+    private Schedule schedule;
+
+    private int gridWidth;
+    private int gridHeight;
+    private int numRabbits;
+    private int birthThreshold;
+    private int grassGrowthRate;
+
+    private String[] initParams = {
+            "GridWidth",
+            "GridHeight",
+            "NumRabbits",
+            "BirthThreshold",
+            "GrassGrowthRate"
+    };
+
     public static void main(String[] args) {
 
         System.out.println("Rabbit skeleton");
 
     }
 
-    public void begin() {
-        // TODO Auto-generated method stub
-
-    }
-
-    public String[] getInitParam() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
     public String getName() {
-        // TODO Auto-generated method stub
-        return null;
+        return "Rabbits Grass Simulation";
     }
 
-    public Schedule getSchedule() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
+    /**
+     * This function is called when the button with the two curved arrows is pressed.
+     */
     public void setup() {
         // TODO Auto-generated method stub
 
+    }
+
+    /**
+     * This function is responsible for initializing the simulation when the ‘Initialize’ button
+     * is clicked on the toolbar.
+     */
+    public void begin() {
+        buildModel();
+        buildSchedule();
+        buildDisplay();
+    }
+
+    public void buildModel() {
+    }
+
+    public void buildSchedule() {
+    }
+
+    public void buildDisplay() {
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    /**
+     * This function returns an array of String variables, each one listing the name of a particular
+     * parameter that you want to be available to vary using the RePast control panel.
+     *
+     * @return Array of String variables for control panel
+     */
+    public String[] getInitParam() {
+        return initParams;
+    }
+
+
+    public int getGridWidth() {
+        return gridWidth;
+    }
+
+    public void setGridWidth(int gridWidth) {
+        this.gridWidth = gridWidth;
+    }
+
+    public int getGridHeight() {
+        return gridHeight;
+    }
+
+    public void setGridHeight(int gridHeight) {
+        this.gridHeight = gridHeight;
+    }
+
+    public int getNumRabbits() {
+        return numRabbits;
+    }
+
+    public void setNumRabbits(int numRabbits) {
+        this.numRabbits = numRabbits;
+    }
+
+    public int getBirthThreshold() {
+        return birthThreshold;
+    }
+
+    public void setBirthThreshold(int birthThreshold) {
+        this.birthThreshold = birthThreshold;
+    }
+
+    public int getGrassGrowthRate() {
+        return grassGrowthRate;
+    }
+
+    public void setGrassGrowthRate(int grassGrowthRate) {
+        this.grassGrowthRate = grassGrowthRate;
     }
 }
