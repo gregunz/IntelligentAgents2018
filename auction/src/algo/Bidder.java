a05f86ae548b32195c9e08ba2bd117ee77dfc42a
maxime-delisle <maxime.delisle@epfl.ch>
2018-11-17 14:49:06 +0100

    modified basic bidder

--- auction/src/algo/Bidder.java
+++ auction/src/algo/Bidder.java
@@ -1,5 +1,6 @@
 package algo;
 
+import logist.Measures;
 import logist.agent.Agent;
 import logist.task.Task;
 import logist.task.TaskDistribution;
@@ -11,6 +12,10 @@ public class Bidder {
     private TaskDistribution distribution;
     private Agent agent;
     private long bidTimout;
+    private double ratioMargin = 0.5;
+    private double ratioIncrease = 0.25;
+    private long fixedCost = 1000;
+    private boolean lastTookFixedCost = false;
 
     private Planner planner;
 
@@ -32,9 +37,14 @@ public class Bidder {
      */
     public Long bid(Task task) {
         long marginalCost = this.planner.estimateMarginalCost(task, bidTimout);
-        long expectedProfit = 100;
+        long bid = Math.round((1 + ratioMargin) * marginalCost);
         //TODO do more here, come up with brilliant ideas using distribution and topology
-        return Math.max(0, marginalCost + expectedProfit);
+        if (bid < fixedCost) {
+            lastTookFixedCost = true;
+            return fixedCost;
+        }
+        lastTookFixedCost = false;
+        return bid;
     }
 
     /**
@@ -43,6 +53,17 @@ public class Bidder {
     public void addInfoOfLastAuction(Task previous, int winner, Long[] bids) {
         if (agent.id() == winner) { // we took the task
             this.planner.addTask(previous);
+            if (lastTookFixedCost) {
+                fixedCost += fixedCost/2;
+            } else {
+                ratioMargin += ratioIncrease;
+            }
+        } else {
+            if (lastTookFixedCost) {
+                fixedCost /= 2;
+            } else {
+                ratioMargin -= 2*ratioIncrease;
+            }
         }
         //TODO we might improve bidder here by learning the other agents ?
     }
