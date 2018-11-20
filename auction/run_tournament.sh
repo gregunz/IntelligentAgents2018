7e1b07dcd84ddb59e346b50d31b76b321c6dd379
maxime-delisle <maxime.delisle@epfl.ch>
2018-11-20 13:05:01 +0100

    added script for tournament

---
+++ auction/run_tournament.sh
@@ -0,0 +1,7 @@
+mkdir tournament
+java -jar ../logist/logist.jar -new $1 config/agents.xml
+rm tournament/$1/agents.xml
+cp config/agents.xml tournament/$1/agents.xml
+java -jar ../logist/logist.jar -run $1 config/auction.xml
+java -jar ../logist/logist.jar -run $1 config/auction2.xml
+java -jar ../logist/logist.jar -score $2
