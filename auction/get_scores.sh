2117f2ee4c26be50011dbe4215c86423936aca18
Gregoire Clement <gregoire.clement@epfl.ch>
2018-11-21 20:41:19 +0100

    fix of cities

--- auction/get_scores.sh
+++ auction/get_scores.sh
@@ -1,5 +1,5 @@
-java -jar ../logist/logist.jar -run $1 config/auction_15.xml
-java -jar ../logist/logist.jar -run $1 config/auction_20.xml
-java -jar ../logist/logist.jar -run $1 config/auction_30.xml
-java -jar ../logist/logist.jar -run $1 config/auction_50.xml
+java -jar ../logist/logist.jar -run $1 config/auction15.xml
+java -jar ../logist/logist.jar -run $1 config/auction20.xml
+java -jar ../logist/logist.jar -run $1 config/auction30.xml
+java -jar ../logist/logist.jar -run $1 config/auction50.xml
 java -jar ../logist/logist.jar -score $1 $2
--- auction/run_tournament.sh
+++ auction/run_tournament.sh
@@ -2,8 +2,8 @@ mkdir -p tournament
 java -jar ../logist/logist.jar -new $1 config/agents.xml
 rm tournament/$1/agents.xml
 cp config/agents.xml tournament/$1/agents.xml
-java -jar ../logist/logist.jar -run $1 config/auction_15.xml
-java -jar ../logist/logist.jar -run $1 config/auction_20.xml
-java -jar ../logist/logist.jar -run $1 config/auction_30.xml
-java -jar ../logist/logist.jar -run $1 config/auction_50.xml
+java -jar ../logist/logist.jar -run $1 config/auction15.xml
+java -jar ../logist/logist.jar -run $1 config/auction20.xml
+java -jar ../logist/logist.jar -run $1 config/auction30.xml
+java -jar ../logist/logist.jar -run $1 config/auction50.xml
 java -jar ../logist/logist.jar -score $1 $2
