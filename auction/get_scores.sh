9c4946925a3697479433cf7b1e20d604cf44ada7
Gregoire Clement <gregoire.clement@epfl.ch>
2018-11-21 23:46:33 +0100

    time for advPlanner reduced

--- auction/get_scores.sh
+++ auction/get_scores.sh
@@ -1,5 +1,3 @@
 java -jar ../logist/logist.jar -run $1 config/auction15.xml
 java -jar ../logist/logist.jar -run $1 config/auction20.xml
-java -jar ../logist/logist.jar -run $1 config/auction30.xml
-java -jar ../logist/logist.jar -run $1 config/auction50.xml
 java -jar ../logist/logist.jar -score $1 $2
