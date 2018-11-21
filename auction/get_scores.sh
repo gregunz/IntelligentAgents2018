4062ae61c8d7b0d1753a86be633a6c3cfb02805c
maxime-delisle <maxime.delisle@epfl.ch>
2018-11-21 16:18:38 +0100

    added script for when the tournament already exists

---
+++ auction/get_scores.sh
@@ -0,0 +1,5 @@
+java -jar ../logist/logist.jar -run $1 config/auction.xml
+java -jar ../logist/logist.jar -run $1 config/auction2.xml
+java -jar ../logist/logist.jar -run $1 config/auction3.xml
+java -jar ../logist/logist.jar -run $1 config/auction4.xml
+java -jar ../logist/logist.jar -score $1 $2
