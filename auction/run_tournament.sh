d45be227caa92989f9c07736461572d014209cc0
maxime-delisle <maxime.delisle@epfl.ch>
2018-11-21 16:24:37 +0100

    added configs

--- auction/run_tournament.sh
+++ auction/run_tournament.sh
@@ -4,4 +4,5 @@ rm tournament/$1/agents.xml
 cp config/agents.xml tournament/$1/agents.xml
 java -jar ../logist/logist.jar -run $1 config/auction.xml
 java -jar ../logist/logist.jar -run $1 config/auction2.xml
+java -jar ../logist/logist.jar -run $1 config/auction3.xml
 java -jar ../logist/logist.jar -score $1 $2
