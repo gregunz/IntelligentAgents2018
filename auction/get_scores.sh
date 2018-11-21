1499a5d514096b3ff4746dae984040968fb0db93
Gregoire Clement <gregoire.clement@epfl.ch>
2018-11-21 20:35:08 +0100

    configs changed

--- auction/get_scores.sh
+++ auction/get_scores.sh
@@ -1,5 +1,5 @@
-java -jar ../logist/logist.jar -run $1 config/auction.xml
-java -jar ../logist/logist.jar -run $1 config/auction2.xml
-java -jar ../logist/logist.jar -run $1 config/auction3.xml
-java -jar ../logist/logist.jar -run $1 config/auction4.xml
+java -jar ../logist/logist.jar -run $1 config/auction_15.xml
+java -jar ../logist/logist.jar -run $1 config/auction_20.xml
+java -jar ../logist/logist.jar -run $1 config/auction_30.xml
+java -jar ../logist/logist.jar -run $1 config/auction_50.xml
 java -jar ../logist/logist.jar -score $1 $2
--- auction/run_tournament.sh
+++ auction/run_tournament.sh
@@ -1,8 +1,9 @@
-mkdir tournament
+mkdir -p tournament
 java -jar ../logist/logist.jar -new $1 config/agents.xml
 rm tournament/$1/agents.xml
 cp config/agents.xml tournament/$1/agents.xml
-java -jar ../logist/logist.jar -run $1 config/auction.xml
-java -jar ../logist/logist.jar -run $1 config/auction2.xml
-java -jar ../logist/logist.jar -run $1 config/auction3.xml
+java -jar ../logist/logist.jar -run $1 config/auction_15.xml
+java -jar ../logist/logist.jar -run $1 config/auction_20.xml
+java -jar ../logist/logist.jar -run $1 config/auction_30.xml
+java -jar ../logist/logist.jar -run $1 config/auction_50.xml
 java -jar ../logist/logist.jar -score $1 $2
